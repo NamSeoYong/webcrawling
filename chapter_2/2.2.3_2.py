d546ae59df823fb07e7c2ada248690cdf14257f7
Seoyong Nam <namsyong0508@hanyang.ac.kr>
2023-07-14 16:09:08 +0900

    add chapter 2

---
+++ chapter_2/2.2.3_2.py
@@ -0,0 +1,10 @@
+# 형제 다루기
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen("http://www.pythonscraping.com/pages/page3.html")
+bs = BeautifulSoup(html, 'html.parser')
+
+for sibling in bs.find('table', {'id': 'giftList'}).tr.next_sibling:
+    print(sibling)
